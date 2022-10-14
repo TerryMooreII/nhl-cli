ded16768fa6b28901508ee09e130b07743b2d651
Terry Moore II <terry.moore.ii@gmail.com>
2022-10-14 10:42:38 -0400

    Utils clean up

--- src/utils.js
+++ src/utils.js
@@ -4,8 +4,8 @@ exports.DATE_FORMAT = 'YYYY-MM-DD';
 
 exports.DATE_FORMAT_LONG = 'dddd, MMMM DD';
 
-exports.padEnd = (name = '', len = 1) => `${name}`.padEnd(len, ' ');
+exports.padEnd = (value = '', len = 1, char = ' ') => `${value}`.padEnd(len, char);
 
-exports.padStart = (name = '', len = 1) => `${name}`.padStart(len, ' ');
+exports.padStart = (value = '', len = 1, char = ' ') => `${value}`.padStart(len, char);
 
 exports.displayLine = (len = 1, char = '-') => console.log(''.padEnd(len, char).brightWhite);
